1c644e9861131bfe115619750b45fb56b1303c84
elsiemaria-012 <108334259+elsiemaria-012@users.noreply.github.com>
2022-11-29 23:07:28 +0100

    my first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+
+
+
+
+// document.querySelector('.form').onclick = function(){
+//     for (let form = 0; form <=1; form++) { 
+//         console('Enter task');
+//       }
+// }
+    
+let task = document.getElementById('task').value;
+console.log(task)
