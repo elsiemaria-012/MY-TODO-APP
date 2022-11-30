df560e16b7e3e844f16553c81c9782e567c82c3b
elsiemaria-012 <108334259+elsiemaria-012@users.noreply.github.com>
2022-11-30 14:46:07 +0100

    mysecondcommit

--- App.js
+++ App.js
@@ -1,12 +1,45 @@
 
 
 
+document.querySelector('.addtoTask').onclick = function(){
+    if (document.querySelector('#newtask input').value.length !== 0){
+        alert("Please Inpute a Task")
+    }   
+}
 
-// document.querySelector('.form').onclick = function(){
-//     for (let form = 0; form <=1; form++) { 
-//         console('Enter task');
-//       }
-// }
-    
-let task = document.getElementById('task').value;
-console.log(task)
+//DEFINATIONS
+
+let input = document.querySelector('.input-bar');
+let addbutton = document.querySelector('.addtoTask');
+let divtask = document.querySelector('.tasks');
+ 
+//ACTIVE AND DISABLE BUTTONS
+
+input.addEventListener('keyup', ()=> {
+    if (input.value.trim() !=0){
+        addbutton.classList.add('active')
+    }
+    else {
+        addbutton.classList.remove('.active')
+    }
+})
+
+//ADDING LIST ITEMS TO LIST
+
+addbutton.addEventListener('click', ()=> {
+    if(input.value.trim() !=0){
+        let newItem = document.createElement('div');
+        newItem.classList.add('item');
+        newItem.innerHTML = `
+        <p> ${ input.value }</p>
+        <div class="item-btn">
+            <i class="fa-sharp fa-solid fa-pencil"></i>
+            <i class="fa-solid fa-xmark"></i>
+        </div>`
+
+        trasks.appendChild(newItem);
+        input.value = '';
+    } else {
+        alert ('Please enter a Task')
+    }
+})
