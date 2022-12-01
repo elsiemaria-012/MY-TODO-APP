e72901b29e82553b852c0fdbfdc824119bb6e807
elsiemaria-012 <108334259+elsiemaria-012@users.noreply.github.com>
2022-12-01 05:42:44 +0100

    initial commit

--- App.js
+++ App.js
@@ -1,22 +1,15 @@
 
-
-
-document.querySelector('.addtoTask').onclick = function(){
-    if (document.querySelector('#newtask input').value.length !== 0){
-        alert("Please Inpute a Task")
-    }   
-}
-
 //DEFINATIONS
 
-let input = document.querySelector('.input-bar');
-let addbutton = document.querySelector('.addtoTask');
-let divtask = document.querySelector('.tasks');
- 
+const input = document.querySelector('.input-bar');
+const addbutton = document.querySelector('.addtoTask');
+const divtask = document.querySelector('.tasks');
+
+
 //ACTIVE AND DISABLE BUTTONS
 
 input.addEventListener('keyup', ()=> {
-    if (input.value.trim() !=0){
+    if (input.value.trim() !==0){
         addbutton.classList.add('active')
     }
     else {
@@ -24,22 +17,36 @@ input.addEventListener('keyup', ()=> {
     }
 })
 
-//ADDING LIST ITEMS TO LIST
 
-addbutton.addEventListener('click', ()=> {
-    if(input.value.trim() !=0){
-        let newItem = document.createElement('div');
-        newItem.classList.add('item');
-        newItem.innerHTML = `
-        <p> ${ input.value }</p>
-        <div class="item-btn">
-            <i class="fa-sharp fa-solid fa-pencil"></i>
-            <i class="fa-solid fa-xmark"></i>
-        </div>`
+//ADDING / INPUTING TASKS
 
-        trasks.appendChild(newItem);
-        input.value = '';
-    } else {
-        alert ('Please enter a Task')
+document.querySelector('.addtoTask').onclick = function(){
+    if (document.querySelector('#newtask input').value.length == 0){
+        alert("Please Inpute a Task")
+    }
+    else{
+        document.querySelector('.tasks').innerHTML +=
+        `<p>${input.value}</p>
+        <div class = "icon-btn">
+        <i class="fa-sharp fa-solid fa-pencil"></i>
+        <i class="fa-solid fa-xmark"></i>`
+    } 
+}
+
+//DELETE ITEMS FROM LISTS
+
+
+tasks.addEventListener('click', (e) => {
+    if (e.target.classList.contains('fa-xmark')){
+        e.target.parentElement.parentElement.remove();
     }
 })
+
+//COMPLETED
+
+tasks.addEventListener('click', (e) => {
+    if (e.target.classList.contains('fa-solid fa-pencil')){
+        e.target.parentElement.parentElement.toggle(completed  );
+    }
+})
+
