4e9df97c5a992a70101e5cae0238e8978e2c7016
elsiemaria-012 <108334259+elsiemaria-012@users.noreply.github.com>
2022-12-06 14:05:02 +0100

    MY-4th-commit

--- App.js
+++ App.js
@@ -4,9 +4,15 @@
 const input = document.querySelector('.input-bar');
 const addbutton = document.querySelector('.addtoTask-btn');
 const divtask = document.querySelector('.tasks');
+//SAVING VALUE AS ARRAY  IN LOCALSTORAGE
 const tasksCollection = [];
-const completed = document.querySelector('.fa-sharp');
-const remove = document.querySelector('.fa-xmark');
+//
+//1. const completed = document.querySelector('.fa-sharp');
+//2. const remove = document.querySelector('.fa-xmark');
+//3. const clearall = document.querySelector(".clearall");
+const currenttask = document.querySelectorAll('.clearall');
+const completedtask = document.querySelectorAll('.itemBlock.completed');
+
 
 
 //ACTIVE AND DISABLE BUTTONS
@@ -17,6 +23,7 @@ input.addEventListener('keyup', ()=> {
     }
     else {
         addbutton.classList.remove('.active')
+        
     }
 })
 
@@ -25,14 +32,14 @@ input.addEventListener('keyup', ()=> {
 
 document.querySelector('.addtoTask-btn').onclick = function(){
     if (document.querySelector('#newtask input').value.length == 0){
-        alert("Please Inpute a Task")
+        alert("Please Input a Task")
     }
     else{
         tasksCollection.push(input.value)
         document.querySelector('.tasks').innerHTML +=
         `
         <div class="icon-btn">
-        <p>${input.value}</p>
+        <p class="parag">${input.value}</p>
         <div>
         <i class="fa-sharp fa-solid fa-pencil"></i>
         <i class="fa-solid fa-xmark"></i>
@@ -48,18 +55,41 @@ document.querySelector('.addtoTask-btn').onclick = function(){
     } 
 };
 
-       //COMPLETED
+// 4.       //COMPLETED
 
-       tasks.addEventListener('click', (e) => {
-           if (e.target.classList.contains('fa-solid fa-pencil')){
-        window.localStorage.removeItem('fa-solid fa-pencil');
-      }
- });
+//        tasks.addEventListener('click', (e) => {
+//            if (e.target.classList.contains('fa-solid fa-pencil')){
+//         window.localStorage.removeItem('parag');
+//       }
+//  });
 
-      // DELETE ITEMS FROM LISTS
+//       // DELETE ITEMS FROM LISTS
 
-        tasks.addEventListener('click', (e) => {
-            if (e.target.classList.contains('fa-xmark')){
-                window.localStorage.removeItem('fa-solid fa-pencil');
-   }
-});
+//         tasks.addEventListener('click', (e) => {
+//             if (e.target.classList.contains('fa-xmark')){
+//                 window.localStorage.removeItem('itemBlock');
+//    }
+// });
+// // CLEAR-ALL ITEMS FROM LISTS
+
+// tasks.addEventListener('click', (e) => {
+//     if (e.target.classList.contains('clearall')){
+//         window.localStorage.clear('iclearall');
+// }
+// });
+for (var i = 0; i <currenttask.length; i++){
+    currenttask[i].onclick = function (){
+        this.parentNode.remove();
+    }
+};
+
+for(var i = 0; i < tasksCollection.length; i++){
+    completedtask[i].onclick = function (){
+        this.classList.toggle('itemBlock.completed');
+    }
+};
+
+
+
+
+
